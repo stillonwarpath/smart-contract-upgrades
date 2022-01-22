aa95bfbc5a47c43d60ca46eeddd70a32b0772bbb
francisco <fr.betancourt@live.com>
2022-01-22 18:01:44 -0300

    Added function for encoding the function call for working with an initializer.

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,5 @@
 from brownie import accounts, network, config
+import eth_utils
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
 
@@ -11,3 +12,23 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+
+# initializer=box.store, 1
+def encode_function_data(initializer=None, *args):
+    """Encodes the function call so we can work with an initializer.
+
+    Args:
+        initializer ([brownie.network.contract.ContractTx], optional):
+        The initializer function we want to call. Example: `box.store`.
+        Defaults to None.
+
+        args (Any, optional):
+        The arguments to pass to the initializer function
+
+    Returns:
+        [bytes]: Return the encoded bytes.
+    """
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
