e93ac47635aaf2f2c5c8aa27a4dc4d74a42c449f
francisco <fr.betancourt@live.com>
2022-01-22 18:27:20 -0300

    Deployed proxy.

--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account, encode_function_data
-from brownie import network, Box, ProxyAdmin
+from brownie import network, Box, ProxyAdmin, TransparentUpgradeableProxy
 
 
 def main():
@@ -9,3 +9,9 @@ def main():
     proxy_admin = proxyAdmin.deploy({"from": account})
     # initializer = box.store, 1
     box_encoded_initializer_function = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1000000},
+    )
