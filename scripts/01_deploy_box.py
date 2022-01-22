9cf06a6131353f5518cf4ae55831da4e19436b86
francisco <fr.betancourt@live.com>
2022-01-22 17:31:05 -0300

    Deployed Box contract.

---
+++ scripts/01_deploy_box.py
@@ -0,0 +1,9 @@
+from scripts.helpful_scripts import get_account
+from brownie import network, Box
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account})
+    print(box.retrieve())
