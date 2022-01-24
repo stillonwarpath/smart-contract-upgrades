5d022ced7fdce00887b69c85e78d96e430a92cd0
francisco <fr.betancourt@live.com>
2022-01-24 17:17:34 -0300

    Deployed BoxV2 contract. Swap of BoxV2 contract.

--- tests/test_proxy_upgrades.py
+++ tests/test_proxy_upgrades.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account, encode_function_data
-from brownie import Box, ProxyAdmin, TransparentUpgradeableProxy
+from brownie import Box, ProxyAdmin, TransparentUpgradeableProxy, Contract, BoxV2
 
 
 def test_proxy_upgrades():
@@ -10,3 +10,7 @@ def test_proxy_upgrades():
     proxy = TransparentUpgradeableProxy.deploy(
         box.address, proxy_admin.address, box_encoded_initializer_function
     )
+
+    # deploy boxV2
+    box_v2 = BoxV2.deploy({"from": account})
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
